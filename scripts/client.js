2b73a56d24f7e5eb5d1b2deef61c543849a98881
Myron <dubtwee@gmail.com>
2021-01-21 22:02:42 -0600

    adding lecture code

--- scripts/client.js
+++ scripts/client.js
@@ -1,7 +1,36 @@
 console.log('Hello Cullen');
 
+const basket = [
+  { name: 'Apple' },
+  { name: 'Orange' }
+];
 // Is the DOM ready?
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('READY');
+  // event listeners
+  $('#js-addFruit').on('click', clickAddFruitHandler);
+  render();
+}
 
 // EVENT HANDLERS
+function clickAddFruitHandler() {
+  console.log('click me')
+  // want input value
+  const fruitName = $('#js-fruit').val();
+  basket.push({ name: fruitName });
+  console.log(basket);
+  render();
+}
 
 // RENDER TO THE DOCUMENT
+function render() {
+  const $fruitList = $('#js-basket');
+
+  $fruitList.empty();
+  for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+    $fruitList.append(`<li>${basket[i].name}</li>`);
+  }
+}
